1f7def95018c61d1359e2512bd1301524a5fa03e
youtidus <youtidus@gmail.com>
2023-09-26 04:18:33 +0900

    試しに移動

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <p>© 2023 My Profile</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>Welcome to My Profile</h1>
+    </header>
+  );
+};
+
+export default Header;
