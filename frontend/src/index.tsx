5313c0b23cbf88add0355548654b87fe6b2c3e7a
youtidus <youtidus@gmail.com>
2023-09-26 09:16:05 +0900

    Add build folder

--- frontend/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from "react-dom";
-import App from "./App";
-
-const container = document.getElementById("root");
-ReactDOM.hydrate(<App />, container);
--- frontend/src/types.ts
+++
@@ -1,6 +0,0 @@
-export type ProfileTypes = {
-  name: string;
-  profession: string;
-  score: number;
-  email: string;
-};
